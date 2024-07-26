f16c58244e4aa464bf8caa3ee9ca68d8d5edc742
warukoff <118911112+warukoff@users.noreply.github.com>
2024-07-27 01:06:08 +0700

    calculator with no functionality

---
+++ calculator 1.0.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::string number;
+    std::cout << "Calculator 1.0" << std::endl ;
+    std::cout << "Введите операцию с числами: ";
+    std::cin >> number;
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
